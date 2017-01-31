df37d48a92b235691a2ada8bc61e411bd0f7c1bd
Colin Oakley <colin@htmlandbacon.com>
2017-01-31 09:41:27 +0000

    updated tests

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,9 +1,9 @@
 const {assert} = require('chai');
 const superagent = require('superagent');
 const status = require('http-status');
-
+// include server
 const server = require('../index');
-
+// ser variables 
 const port = process.env.PORT || 3000;
 const validUserID = 'A0001';
 const invalidUserID = 'B0001';
@@ -12,15 +12,19 @@ describe('/api/users', function() {
     let app;
  
     before(function() {
+        // start server
         app = server(port);
     });
     
     after(function() {
+        // close server
         app.close();
     });
     
-    it('should return list of users when called', function(done) {
-        superagent.get(`http://localhost:${port}/api/users`).end(function(err, res) {
+    it('should return list of users when called', function(done) {  
+        superagent
+        .get(`http://localhost:${port}/api/users`)
+        .end(function(err, res) {
             assert.ifError(err);
             assert.equal(res.status, status.OK);
             const result = JSON.parse(res.text);
@@ -30,19 +34,23 @@ describe('/api/users', function() {
     });
     describe('/:id', function() { 
         it('returns user when valid user id is used', function(done) {
-                superagent.get(`http://localhost:${port}/api/users/${validUserID}`).end(function(err, res) {
+            superagent
+            .get(`http://localhost:${port}/api/users/${validUserID}`)
+            .end(function(err, res) {
                 assert.ifError(err);
                 assert.equal(res.status, status.OK);
-                var result = JSON.parse(res.text);
+                const result = JSON.parse(res.text);
                 assert.equal(result.id, validUserID);
                 done();
             });
         });
 
         it('returns 404 error with message when user does not exist', function(done) {
-                superagent.get(`http://localhost:${port}/api/users/${invalidUserID}`).end(function(err, res) {
+            superagent
+            .get(`http://localhost:${port}/api/users/${invalidUserID}`)
+            .end(function(err, res) {
                 assert.equal(res.status, status.NOT_FOUND);
-                var result = JSON.parse(res.text);
+                const result = JSON.parse(res.text);
                 assert.equal(result.message, `User ${invalidUserID} was not found.`);
                 done();
             });
